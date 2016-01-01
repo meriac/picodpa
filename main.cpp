a957c3ce7e5c675b1136f9e7baf203ff72f26b65
Milosch Meriac <milosch@meriac.com>
2016-01-01 21:46:55 +0000

    switched output to stderr, added filtering

--- main.cpp
+++ main.cpp
@@ -7,11 +7,16 @@
 
 #define MHZ(x) ((x)*1000000.0)
 #define SAMPLE_FREQUENCY MHZ(62.5)
-#define SAMPLE_INTERVAL_PS (int)(((1000000000000.0/SAMPLE_FREQUENCY)+0.5))
+#define SAMPLE_INTERVAL_NS (int)(((1000000000.0/SAMPLE_FREQUENCY)+0.5))
+#define FILTER_FREQUENCY 1000.0
+#define FILTER_INTERVAL_NS (int)(((1000000000.0/FILTER_FREQUENCY)+0.5))
 
 #define CHANNELS 1
 
 static volatile bool g_terminate;
+static uint32_t g_sample_interval_ns;
+static int g_oversampling;
+static volatile int g_buffer_count;
 
 void samples_ready(
     int16_t  handle,
@@ -24,8 +29,22 @@ void samples_ready(
     void     *pParameter
 )
 {
-    printf("idx=%08i overflow=%i samples=%08i\n",
-        startIndex, overflow, noOfSamples);
+    int16_t *buffer;
+    static int progress;
+
+    buffer = ((int16_t*)pParameter)+startIndex;
+    while(noOfSamples--)
+    {
+        g_oversampling += *buffer++;
+        if(progress < FILTER_INTERVAL_NS)
+            progress += g_sample_interval_ns;
+        else
+        {
+            progress -= FILTER_INTERVAL_NS;
+            g_oversampling = 0;
+            g_buffer_count++;
+        }
+    }
 }
 
 void shutdown_request(int sig){
@@ -36,7 +55,6 @@ int main(int argc, char *argv[])
 {
     PICO_STATUS status;
     int i, max_samples, block_us;
-    uint32_t sample_interval;
     int16_t hscope;
     int16_t *buffer;
 
@@ -46,7 +64,7 @@ int main(int argc, char *argv[])
     /* open first available scope */
     if((status = ps3000aOpenUnit(&hscope, NULL)) != PICO_OK)
     {
-        printf("Picoscope device open failed [0x%x]\n",(uint32_t)status);
+        fprintf(stderr,"Picoscope device open failed [0x%x]\n",(uint32_t)status);
         return 1;
     }
 
@@ -59,7 +77,7 @@ int main(int argc, char *argv[])
         PS3000A_5V,
         0)) != PICO_OK)
     {
-        printf("failed to configure channel [0x%X]\n",(uint32_t)status);
+        fprintf(stderr,"failed to configure channel [0x%X]\n",(uint32_t)status);
         return 2;
     }
 
@@ -72,22 +90,22 @@ int main(int argc, char *argv[])
         PS3000A_5V,
         0)) != PICO_OK)
     {
-        printf("failed to configure channel [0x%x]\n",(uint32_t)status);
+        fprintf(stderr,"failed to configure channel [0x%x]\n",(uint32_t)status);
         return 3;
     }
 
     /* enable dual-buffering */
     if((status = ps3000aMemorySegments(hscope, CHANNELS, &max_samples)) != PICO_OK)
-        printf("Failed to set memory segment count [0x%X]\n",(uint32_t)status);
+        fprintf(stderr,"Failed to set memory segment count [0x%X]\n",(uint32_t)status);
     else
     {
-        printf("allocating %i samples for buffer...\n", max_samples);
+        fprintf(stderr,"allocating %i samples for buffer...\n", max_samples);
 
         /* allocate buffer memories */
         buffer = (int16_t*)
             malloc(max_samples * CHANNELS * sizeof(buffer[0]));
         if(!buffer)
-            printf("ERROR: failed to allocate memory!\n");
+            fprintf(stderr,"ERROR: failed to allocate memory!\n");
         else
         {
             /* assign driver buffers to buffer memories */
@@ -101,29 +119,29 @@ int main(int argc, char *argv[])
                     PS3000A_RATIO_MODE_NONE)) != PICO_OK)
                     break;
             if(i!=CHANNELS)
-                printf("ERROR: registering channel %i (0x%X)!\n", i+1, status);
+                fprintf(stderr,"ERROR: registering channel %i (0x%X)!\n", i+1, status);
             else
             {
                 /* run data aquisition */
-                sample_interval = SAMPLE_INTERVAL_PS;
+                g_sample_interval_ns = SAMPLE_INTERVAL_NS;
                 if((status = ps3000aRunStreaming(
                     hscope,
-                    &sample_interval,
-                    PS3000A_PS,
+                    &g_sample_interval_ns,
+                    PS3000A_NS,
                     0,
                     max_samples,
                     false,
                     1,
                     PS3000A_RATIO_MODE_NONE,
                     max_samples)) != PICO_OK)
-                    printf("ERROR: failed to run streaming (0x%X)\n", status);
+                    fprintf(stderr,"ERROR: failed to run streaming (0x%X)\n", status);
 
                 /* show actual sampling rate */
-                block_us = (((int64_t)sample_interval)*max_samples)/1000000;
-                printf("buffer covers %ims...\n", block_us/1000);
-                printf("sampling at freqency %.2fMHz (interval %i)...\n",
-                    (1000000.0f / sample_interval),
-                    sample_interval
+                block_us = (g_sample_interval_ns*max_samples)/1000;
+                fprintf(stderr,"buffer covers %ims...\n", block_us/1000);
+                fprintf(stderr,"sampling at freqency %.2fMHz (interval %i)...\n",
+                    (1000.0f / g_sample_interval_ns),
+                    g_sample_interval_ns
                 );
 
                 /* run data aquisition */
@@ -134,16 +152,20 @@ int main(int argc, char *argv[])
                     ) == PICO_OK)
                 {
                     usleep(block_us/4);
+
+                    fprintf(stderr, "\rsamples=%08i", g_buffer_count);
+                    fflush(stderr);
                 }
+                fprintf(stderr,"\n");
 
                 /* stop data acquisition after control-c */
                 if((status = ps3000aStop(hscope)) != PICO_OK)
-                    printf("Failed to stop data acquisition (0x%08X)\n", status);
+                    fprintf(stderr,"Failed to stop data acquisition (0x%08X)\n", status);
             }
         }
     }
 
-    printf("Terminated...\n");
+    fprintf(stderr,"Terminated...\n");
     /* done */
     ps3000aCloseUnit(hscope);
     return 0;
