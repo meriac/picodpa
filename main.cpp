53f0ecdf71fbc25055ceea987df86c4649dd200a
Milosch Meriac <milosch@meriac.com>
2016-01-02 12:55:25 +0000

    added license

--- main.cpp
+++ main.cpp
@@ -1,3 +1,25 @@
+/***************************************************************
+ *
+ * PicoDPA - command line DAQ tool for PicoScope 3204A
+ *
+ * Copyright 2015 Milosch Meriac <milosch@meriac.com>
+ *
+ ***************************************************************/
+
+/*
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as published
+ by the Free Software Foundation; version 3.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with this program; if not, write to the Free Software Foundation,
+ Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
